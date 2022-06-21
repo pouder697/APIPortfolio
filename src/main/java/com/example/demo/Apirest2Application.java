999104ffbd5da5a1541d9a284ea3557563ec283a
andy <andres.genaisir@gmail.com>
2022-06-21 18:16:18 -0300

    nuevo intento CORS

--- src/main/java/com/example/demo/Apirest2Application.java
+++ src/main/java/com/example/demo/Apirest2Application.java
@@ -15,31 +15,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class Apirest2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Apirest2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Apirest2Application.class, args);
+    }
 
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/login").allowedOrigins("http://localhost:4200").allowedMethods("PUT", "GET", "POST");
+            }
+        };
+    }
 
-	@EnableWebSecurity
-	@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests()
-					.antMatchers(HttpMethod.POST, "/api/login").permitAll()
-					.anyRequest().authenticated();
-		}
-	}
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/login").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
+
 }
